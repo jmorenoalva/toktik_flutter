5643833269a0f8c417dee5fe1c7a3edb4c015b4a
Javier Moreno <jmoreno@sintad.pe>
2025-01-14 18:50:29 -0500

    Clase 108 - 109 . Gradiente de fondo

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/video/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -64,7 +65,9 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
                 VideoPlayer(controller),
 
                 //Gradiente
-
+                VideoBackground(
+                  stops: const [0.8,1.0]
+                ),
 
                 //Testo
                 Positioned(
