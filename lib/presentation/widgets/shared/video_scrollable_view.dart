ccca3df99887a6dff05eafb7142b9bafdae8cdb0
Javier Moreno <jmoreno@sintad.pe>
2025-01-13 19:11:02 -0500

    Clase 103-104. Video Player

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
 
@@ -23,6 +24,12 @@ class VideoScrollableView extends StatelessWidget {
         return Stack(
           children:[
             //Video Player + gradientes
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                caption: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+              )
+            ),
 
             //Botones
             Positioned(
